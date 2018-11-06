9e112c57260d047c18e4ee65dd0a8146f9fb095e
Sergiro Z <zujonok@hotmail.com>
2018-11-06 23:46:26 +0300

    Добавьте файлы проекта.

---
+++ dj_cours/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Сергей\Documents\Academy\Django\dj_cours\dj_cours\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
